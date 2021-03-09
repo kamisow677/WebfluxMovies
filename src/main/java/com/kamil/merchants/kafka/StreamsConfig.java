ba6dad574aa6851f4674f71c6f1eaa45b4d224b5
kamisow677 <k.sowa181@gmail.com>
2021-03-09 21:16:55 +0100

    delete unused files

--- src/main/java/com/kamil/merchants/kafka/StreamsConfig.java
+++
@@ -1,9 +0,0 @@
-package com.kamil.merchants.kafka;
-
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.stereotype.Component;
-
-@Component
-@EnableBinding(MoviesStream.class)
-public class StreamsConfig {
-}
